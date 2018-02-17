7e0c289a91ae6d92505daa654292cef77ee9eadd
Jerome Wu <jeromewus@gmail.com>
2018-02-17 22:29:48 +0800

    Add logo, server selection buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
 import Background from './Background';
+import logo from '../assets/images/logo.png';
 
+const LogoImage = styled.img`
+  width: 90%;
+
+  @media only screen and (min-width: 768px) {
+    width: 551px;
+  }
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
 
 const App = () => (
-  <Background />
+  <Background>
+    <LogoImage src={logo} alt="logo" />
+    <h3>Event Calculator</h3>
+    <h4>Select server:</h4>
+    <Buttons>
+      <Button basic content="Japan" />
+      <Button basic content="Simplified Chinese" />
+      <Button basic content="Traditional Chinese" />
+      <Button basic content="English" />
+    </Buttons>
+  </Background>
 );
 
 export default App;
--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,11 @@ const fromColor = 'rgba(0, 0, 0, 0)';
 const toColor = 'rgba(0, 0, 0, 0.02)';
 
 export default styled.div`
+  display: flex;
+  flex-direction: column;
   flex: 1;
+  justify-content: center;
+  align-items: center;
   background-color: ${fromColor};
   background-image: linear-gradient(
     45deg,
