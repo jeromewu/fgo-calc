ebae14df1a5aa3a7f5bc371dcab03e20e3c7d9cd
Jerome Wu <jeromewus@gmail.com>
2018-02-18 00:12:25 +0800

    Add redux, react-router and enable hot reload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
+import { Provider } from 'react-redux';
 import logo from 'assets/images/logo.png';
 import Background from 'components/Background';
 import LogoImage from './LogoImage';
 import Buttons from './Buttons';
 
-const App = () => (
-  <Background>
-    <LogoImage src={logo} alt="logo" />
-    <h3>Event Calculator</h3>
-    <h4>Select server:</h4>
-    <Buttons>
-      <Button basic content="Japan" />
-      <Button basic content="Simplified Chinese" />
-      <Button basic content="Traditional Chinese" />
-      <Button basic content="English" />
-    </Buttons>
-  </Background>
+const App = ({ store }) => (
+  <Provider store={store}>
+    <Background>
+      <LogoImage src={logo} alt="logo" />
+      <h3>Event Calculator</h3>
+      <h4>Select server:</h4>
+      <Buttons>
+        <Button basic content="Japan" />
+        <Button basic content="Simplified Chinese" />
+        <Button basic content="Traditional Chinese" />
+        <Button basic content="English" />
+      </Buttons>
+    </Background>
+  </Provider>
 );
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable global-require */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import App from './components/App';
+import store from './store';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  const App = require('./components/App').default;
+  ReactDOM.render(<App store={store} />, document.getElementById('root'));
+};
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept(() => {
+    setImmediate(() => render());
+  });
+}
+
+render();
+
 registerServiceWorker();
