7ded6822b6d681ea5d9e56b536db38c610006c65
Jerome Wu <jeromewus@gmail.com>
2018-02-17 21:41:14 +0800

    Restructure folder and add background image

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Background from './Background';
+
+
+const App = () => (
+  <Background />
+);
+
+export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const fromColor = 'rgba(0, 0, 0, 0)';
+const toColor = 'rgba(0, 0, 0, 0.02)';
+
+export default styled.div`
+  flex: 1;
+  background-color: ${fromColor};
+  background-image: linear-gradient(
+    45deg,
+    ${fromColor} 25%,
+    ${toColor} 25%,
+    ${toColor} 75%,
+    ${fromColor} 75%,
+    ${fromColor}
+  ),
+  linear-gradient(
+    -45deg,
+    ${fromColor} 25%,
+    ${toColor} 25%,
+    ${toColor} 75%,
+    ${fromColor} 75%,
+    ${fromColor}
+  );
+  background-size: 50px 50px;
+`;
