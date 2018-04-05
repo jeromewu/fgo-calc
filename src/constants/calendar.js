a01c7ba7e4d24fc6f29d650df13128fee1cefb45
Jerome Wu <jeromewus@gmail.com>
2018-04-05 15:31:06 +0800

    Fix safari date format issue

--- src/constants/calendar.js
+++ src/constants/calendar.js
@@ -4,30 +4,30 @@ export default {
   tc: [
     {
       name: 'FGO2016Summer',
-      start: (new Date('2018-04-03T16:00:00+0800')).getTime(),
-      end: (new Date('2018-04-24T12:59:00+0800')).getTime(),
+      start: 1522742400000, // 2018-04-03T16:00:00+0800
+      end: 1524545940000, // 2018-04-24T12:59:00+0800
     },
     {
       name: 'Onigashima',
-      start: (new Date('2018-03-08T16:00:00+0800')).getTime(),
-      end: (new Date('2018-03-22T12:59:00+0800')).getTime(),
+      start: 1520496000000, // 2018-03-08T16:00:00+0800
+      end: 1521694740000, // 2018-03-22T12:59:00+0800
     },
     {
       name: 'JourneyToTheWest',
-      start: (new Date('2018-02-12T16:00:00+0800')).getTime(),
-      end: (new Date('2018-02-26T12:59:00+0800')).getTime(),
+      start: 1518422400000, // 2018-02-12T16:00:00+0800
+      end: 1519621140000, // 2018-02-26T12:59:00+0800
     },
     {
       name: 'DaVinciAndThe7CounterfeitHeroicSpirits',
-      start: (new Date('2017-12-14T16:00:00+0800')).getTime(),
-      end: (new Date('2017-12-24T12:59:00+0800')).getTime(),
+      start: 1513238400000, // 2017-12-14T16:00:00+0800
+      end: 1514091540000, // 2017-12-24T12:59:00+0800
     },
   ],
   en: [
     {
       name: 'DaVinciAndThe7CounterfeitHeroicSpirits',
-      start: (new Date('2018-04-04T02:00:00-0700')).getTime(),
-      end: (new Date('2018-04-13T20:59:00-0700')).getTime(),
+      start: 1522832400000, // 2018-04-04T02:00:00-0700
+      end: 1523678340000, // 2018-04-13T20:59:00-0700
     },
   ],
 };
